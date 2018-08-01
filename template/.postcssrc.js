214c27a70cc05d09c617d0a8153322f8577d9cf8
louis wen <75168611@qq.com>
2018-08-01 14:59:24 +0800

    Update .postcssrc.js

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -1,11 +1,17 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    'cssnano': {}
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
+    plugins: [
+        //自动添加索引
+        require('autoprefixer')({
+            browsers: ['last 2 versions', 'ie >= 9', '> 5% in CN']
+        }),
+        //压缩样式
+        require('cssnano')({
+            // 关闭cssnano的autoprefixer选项，不然会和前面的autoprefixer冲突
+            autoprefixer: false,
+            reduceIdents: false,
+            zindex: false,
+            discardUnused: false,
+            mergeIdents: false
+        })
+    ]
+};
